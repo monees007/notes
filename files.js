7d0ec08c9ac59cf903d5c57512a6ea2c61af91e9
Manish Chandra <monees007@outlook.com>
2026-03-28 15:21:59 +0530

    Add PDF files to BioEngineering category

--- files.js
+++ files.js
@@ -1,4 +1,19 @@
 const pdfData = {
+  "BioEngineering": [
+    "Biochemistry (Reginald H- G_ (Z-Library).pdf",
+    "Biochemistry (Roger L- Mies_ (Z-Library).pdf",
+    "Developmental Biology, 12th edition Michael J.F. Barresi, Scott F. Gilbert.pdf",
+    "Genomes 4.pdf",
+    "Human Reproductive Biology, Third Edition.pdf",
+    "Lehninger Principles of Biochemistry, 8th ed.pdf",
+    "Life The Science of Biology (Z-Library).pdf",
+    "Machine learning in bioinfo_ (Z-Library).pdf",
+    "Molecular Virology of Human_ (Z-Library).pdf",
+    "Principles of Genetics, 7th edition (2016).pdf",
+    "Textbook of Biochemistry for Medical Students.pdf",
+    "Vertebrates - R.L. Kotpal.pdf",
+    "Viruses and human disease (_ (Z-Library).pdf"
+  ],
   "Glycoconjugates": [
     "Carbohydrate and Protein Metabolism.pdf",
     "Glycoconjugates_Intro.pdf",
